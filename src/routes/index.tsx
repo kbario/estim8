473d429009a441b7855f3e52e9a7b954dc1165ad
kbario <kylebario1@gmail.com>
2023-09-30 11:18:55 +0800

    theme and scaf

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,17 +2,23 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  return (
-    <div>estim8</div>
-  );
+  
+  return <div class="h-screen w-full">
+    <div class="form-control w-fit">
+      <label class="label cursor-pointer gap-4">
+        <span class="label-text">Init</span>
+        <input type="checkbox" class="checkbox" />
+      </label>
+    </div>
+  </div>;
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "estim8",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "estim8 like a pro",
     },
   ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  daisyui: {
+    themes: ["dracula"],
+  },
+  plugins: [require("daisyui")],
   theme: {
     extend: {},
   },
-  plugins: [],
 };
