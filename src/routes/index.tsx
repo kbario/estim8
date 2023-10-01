6bc0dff2c14a4f0ca4b1090e35eddc8cc4ba24c3
kbario <kylebario1@gmail.com>
2023-10-01 09:01:40 +0800

    overall workflow done

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,198 +1,153 @@
-import { $, QRL, Signal, component$, useComputed$, useSignal, useStore } from "@builder.io/qwik";
+import { component$, useComputed$, useStore } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 type Thing = {
   name: string;
-  enabled: Signal<boolean>;
-  hours: Signal<string>;
-  min: number
-}
+  enabled: boolean;
+  hours: string;
+  min: number;
+};
 
 type EnclosedThing = {
   name: string;
   data: Thing[];
-  // addThing: QRL<(this: EnclosedThing) => void>
-}
+};
 
 type BasicProps = {
-  data: Thing
-}
+  data: Thing;
+};
 
-type MyStore = {
-  data: Array<Thing | EnclosedThing>,
-  // addThing: QRL<(this: MyStore) => void>
-  // addEnclosedThing: QRL<(this: MyStore) => void>
-}
+type MyStore = Array<Thing | EnclosedThing>;
 
-const isEnclosedThing = (thing: any): thing is EnclosedThing => Array.isArray(thing?.['data']) && !thing?.['hours']
+const isEnclosedThing = (thing: any): thing is EnclosedThing =>
+  Array.isArray(thing?.["data"]) && !thing?.["hours"];
 
 const arst = (acc: number, idv: Thing | EnclosedThing) => {
   if (isEnclosedThing(idv)) {
-    acc += idv.data.reduce(arst, 0)
+    acc += idv.data.reduce(arst, 0);
   } else {
-    acc += idv.enabled.value ? parseFloat(idv.hours.value) : 0
+    acc += idv.enabled ? parseFloat(idv.hours) || 0 : 0;
   }
-  return acc
+  return acc;
+};
+
+function makeThing(name: string, hours: number, min: number = 1) {
+  return {
+    enabled: true,
+    hours: hours.toString(),
+    min: min,
+    name,
+  };
+}
+function makeEnclosedThing(
+  name: string,
+  data: { name: string; hours: number; min?: number },
+) {
+  return {
+    name,
+    data: [makeThing(data.name, data.hours, data.min || 1)],
+  };
 }
 
 export default component$(() => {
-
-  function addThingToMyStore(this: MyStore) {
-    const enabled = useSignal(true)
-    const hours = useSignal('1')
-    this.data.push(
-      {
-        enabled,
-        hours,
-        min: 1,
-        name: 'Learning Time/Non-project'
-      }
-    )
-  }
-
-  function addThingToEnclosedThing(this: EnclosedThing) {
-    const enabled = useSignal(true)
-    const hours = useSignal('1')
-    this.data.push({
-      enabled,
-      hours,
-      min: 1,
-      name: 'Learning Time/Non-project'
-    })
-  }
-
-  function addEnclosedThingToMyStore(this: MyStore) {
-    const enabled = useSignal(true)
-    const hours = useSignal('1')
-    this.data.push(
-      {
-        name: 'Dev Work',
-        data: [{
-          enabled,
-          hours,
-          min: 1,
-          name: 'Revisions/bug-fixing'
-        }],
-        // addThing: $(addThingToEnclosedThing)
-
-      }
-    )
-  }
-
-  // function makeThing()
-  // $((x: EnclosedThing) => {
-  //
-  //   x.data.push({ name: '', hours: useSignal('0'), enabled: useSignal(true), min: 1 })
-  // })
-
-  const store = useStore<MyStore>({
-    data: [
-      {
-        enabled: useSignal(true),
-        hours: useSignal('1'),
-        min: 1,
-        name: 'Communications & Meetings'
-      },
-      {
-        enabled: useSignal(true),
-        hours: useSignal('1'),
-        min: 1,
-        name: 'Initial Setup'
-      },
-      {
-        name: 'Dev Work',
-        data: [{
-          enabled: useSignal(true),
-          hours: useSignal('2'),
-          min: 1,
-          name: 'Revisions/bug-fixing'
-        }],
-        /* addThing: $(addThingToEnclosedThing) */
-      },
-      // dev placeholder
-      {
-        enabled: useSignal(true),
-        hours: useSignal('2'),
-        min: 1,
-        name: 'Revisions/bug-fixing'
-      },
-      {
-        enabled: useSignal(true),
-        hours: useSignal('4'),
-        min: 1,
-        name: 'Unit Tests'
-      },
-      {
-        enabled: useSignal(true),
-        hours: useSignal('1'),
-        min: 1,
-        name: 'Manual Testing'
-      },
-      {
-        enabled: useSignal(true),
-        hours: useSignal('0.5'),
-        min: 0.5,
-        name: 'Merging & Deployments'
-      },
-      {
-        enabled: useSignal(true),
-        hours: useSignal('1'),
-        min: 0,
-        name: 'Learning Time/Non-project'
-      }
-    ],
-    // addThing: $(addThingToMyStore),
-    // addEnclosedThing: $(addEnclosedThingToMyStore),
-  },
-  )
-
-  // useTask$(({ track }) => {
-  //   track(() => {
-  //     store.enabled
-  //   })
-  //   console.log(store.init.hours.value)
-  // })
+  const store = useStore<MyStore>([
+    makeThing("Communications & Meetings", 1),
+    makeThing("Initial Setup", 1),
+    makeEnclosedThing("Dev Work", { name: "asdf", hours: 2 }),
+    makeThing("Revisions/bug-fixing", 2),
+    makeThing("Unit Tests", 4),
+    makeThing("Manual Testing", 1),
+    makeThing("Merging & Deployments", 0.5),
+    makeThing("Learning Time/Non-project", 1, 0),
+  ]);
 
   const total = useComputed$(() => {
-    return store.data.reduce(arst, 0)
-  })
+    return store.reduce(arst, 0);
+  });
 
-  return <div class="h-screen w-full p-10">
-    {store.data.map(x => (isEnclosedThing(x) ?
-      (
-        <div>
-          {x?.name}
-          <ul>
-            {x?.data.map(y => <li><Basic data={y} /></li>)}
-          </ul>
-          <button class="btn">add</button>
-        </div>
-      )
-      : <Basic data={x} />))}
-    <div>
-      {total.value}
+  return (
+    <div class="h-screen w-full p-10">
+      {store.map((x, idx) =>
+        isEnclosedThing(x) ? (
+          <div class="" key={idx}>
+            {x?.name}
+            <ul>
+              {x?.data.map((y, id) => (
+                <li class="flex items-center gap-4" key={id}>
+                  <Basic data={y} />
+
+                  <button
+                    class="btn btn-error btn-outline"
+                    onClick$={() => x.data.splice(id, 1)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      class="h-6 w-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                  </button>
+                </li>
+              ))}
+            </ul>
+            <button
+              class="btn"
+              onClick$={() =>
+                x.data.push(makeThing(Math.random().toString(), 1))
+              }
+            >
+              add
+            </button>
+          </div>
+        ) : (
+          <Basic key={idx} data={x} />
+        ),
+      )}
+      <div>{total.value}</div>
     </div>
-  </div>;
+  );
 });
-
 
 const Basic = component$<BasicProps>((props) => {
   return (
-    <div class="flex gap-4 w-fit items-center">
+    <div class="flex w-fit items-center gap-4">
       <div class="form-control w-fit">
-        <label class="label cursor-pointer gap-4" >
-          <input type="checkbox" bind: checked={props.data.enabled} class="checkbox" />
+        <label class="label cursor-pointer gap-4">
+          <input
+            type="checkbox"
+            checked={props.data.enabled}
+            onChange$={(_, el) => (props.data.enabled = el.checked)}
+            class="checkbox"
+          />
           <span class="label-text">{props.data.name}</span>
         </label>
       </div>
       <div class="form-control w-fit">
-        <label class="label cursor-pointer gap-4" >
-          <input type="number" inputMode="numeric" pattern="[0-9]*" min={props.data.min} disabled={!props.data.enabled.value} placeholder="number of hours" class="input input-bordered input-primary w-full max-w-xs" bind: value={props.data.hours} />
+        <label class="label cursor-pointer gap-4">
+          <input
+            type="number"
+            inputMode="numeric"
+            pattern="[0-9]*"
+            min={props.data.min}
+            disabled={!props.data.enabled}
+            placeholder="number of hours"
+            class="input input-bordered input-primary w-full max-w-xs"
+            value={props.data.hours}
+            onInput$={(_, el) => (props.data.hours = el.value)}
+          />
         </label>
       </div>
     </div>
-  )
-})
+  );
+});
 
 export const head: DocumentHead = {
   title: "estim8",
